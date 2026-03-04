abcdf5d3abe58cfc45545a2dcf7492bb4aa7f83f
Amit Shinde <shinde13amit@gmail.com>
2026-03-04 02:23:55 +0100

    Initial Commit

---
+++ ReeldTests/ReeldTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReeldTests.swift
+//  ReeldTests
+//
+//  Created by Amit Shinde on 2026-03-04.
+//
+
+import Testing
+@testable import Reeld
+
+struct ReeldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
